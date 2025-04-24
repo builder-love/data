7ef4dae96a7d86e470e08571fd78a916ac57630a
treboryatska <treboryatska@gmail.com>
2025-04-24 17:33:25 +0000

    is_fork dbt models

---
+++ pipelines/dbt-pipelines/dbt_pipelines/models/clean/latest_project_is_fork.sql
@@ -0,0 +1,25 @@
+-- create a table that shows the latest projects and their is_fork status
+-- get ratio of forked/non-forked repos by project
+
+{{ config(
+    materialized='table',
+    unique_key='project_title || data_timestamp',
+    tags=['latest_clean_data']
+) }}
+
+select 
+  o.project_title,
+  count(*) as repo_count,
+  SUM(f.is_fork::INTEGER) AS is_fork_count,
+  COUNT(*) - SUM(f.is_fork::INTEGER) AS is_not_fork_count,
+  SUM(f.is_fork::INTEGER)::NUMERIC / COUNT(*) AS is_fork_ratio,
+  (1 - (SUM(f.is_fork::INTEGER)::NUMERIC / COUNT(*))) AS is_not_fork_ratio,
+  MAX(f.data_timestamp) AS data_timestamp
+
+from {{ ref('latest_project_repos_is_fork') }} f left join {{ ref('latest_project_repos') }} o
+  on f.repo = o.repo
+
+where f.is_fork is not null
+
+group by 1
+order by 4
