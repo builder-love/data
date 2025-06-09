4bb48679d47c67d03ebc9ad2b82b32b700fee2a8
treboryatska <treboryatska@gmail.com>
2025-06-09 17:13:58 +0000

    created dbt models for clean

---
+++ pipelines/dbt-pipelines/dbt_pipelines/models/clean/latest_project_repos_description.sql
@@ -0,0 +1,12 @@
+-- models/clean/latest_project_repos_description.sql
+-- if clean.latest_project_repos_description table exists, replace it with the raw.latest_project_repos_description table
+-- this is a simple replace, but the associated test checks record counts
+
+{{ config(
+    materialized='table',
+    unique_key='repo || data_timestamp',
+    tags=['latest_clean_data']
+) }}
+
+SELECT *
+FROM {{ source('raw', 'latest_project_repos_description') }} 
